cfeecfc86498aa18db3eb26cd0a1f4194fd59775
sichoon.kwon <sichoon.kwon@solbox.com>
2017-06-22 14:15:37 +0900

    security test

---
+++ src/main/java/com/com/test/PathMpTest.java
@@ -0,0 +1,37 @@
+package com.com.test;
+
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by sichoon.kwon on 2017-06-22.
+ */
+public class PathMpTest {
+    public void fileUpLoad(String bbsSeq) {
+        boolean fileUploadCheck = false;
+
+        // bbsSeq는 DB sequence 값이지만 보안진단에 걸려서 안 걸리게 처리
+        boolean b = false;
+        if(bbsSeq != null && !bbsSeq.equals("")) {
+            Pattern p = Pattern.compile("^[0-9]*$");
+            Matcher m = p.matcher(bbsSeq);
+            b = m.matches();
+        }
+
+        if(b) {
+            File uploadDirectory = new File("/usr/service/server/cdn32-adp/webapps/upload/support", bbsSeq); // CHANNEL_ID가 폴더명
+
+            if (!uploadDirectory.exists()) {
+                uploadDirectory.mkdirs();
+                //log.info("uploadDirectory 디렉토리를 생성했습니다.");
+            }
+            System.out.println("fileUpLoad success");
+        }
+    }
+
+    public static void main(String[] args) {
+        PathMpTest pmt = new PathMpTest();
+        pmt.fileUpLoad("1");
+    }
+}
---
+++ src/main/java/com/com/test/XmlEEITest.java
@@ -0,0 +1,65 @@
+package com.com.test;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
+
+/**
+ * Created by sichoon.kwon on 2017-06-22.
+ */
+public class XmlEEITest {
+    public void xmlParseData(String xmlData) throws Exception {
+            // xml가공 처리(xml Document생성)
+            InputSource is = new InputSource(new StringReader(xmlData));
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+            try {
+//                dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+                // XML Entity Expansion Injection 공격 대응
+//                dbf.setFeature("http://javax.xml.XMLConstants/feature/secure-processing", true);
+//                dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+                dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+                dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            } catch (Throwable throwable) {
+            }
+
+            DocumentBuilder dBuilder = dbf.newDocumentBuilder();
+
+            Document doc = dBuilder.parse(is);
+
+            NodeList nodeList = doc.getElementsByTagName("node");
+
+        System.out.println(nodeList.item(0));
+    }
+
+    public static void main(String[] args) {
+        try {
+            XmlEEITest xet = new XmlEEITest();
+            xet.xmlParseData("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                    "<system>\n" +
+                    "  <node>\n" +
+                    "    <count>1</count>\n" +
+                    "    <name>Red Hat, Inc SCSI storage controller 0 channel(s)</name>\n" +
+                    "    <type>raid</type>\n" +
+                    "  </node>\n" +
+                    "  <node>\n" +
+                    "    <count>1</count>\n" +
+                    "    <name>DIMM RAM 1024MB</name>\n" +
+                    "    <type>mem</type>\n" +
+                    "  </node>\n" +
+                    "  <node>\n" +
+                    "    <count>1</count>\n" +
+                    "    <name>Intel Core Processor (Broadwell) 1 core(s)</name>\n" +
+                    "    <type>cpu</type>\n" +
+                    "  </node>\n" +
+                    "</system>");
+        } catch (Exception e) {
+
+        }
+    }
+}
